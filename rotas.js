c4ef68092f55b4817cea868e48ced13ff5806e90
Thiago <thiagofreitas1106@gmail.com>
2022-03-25 15:21:43 -0300

    alterações: tela de login(novo layout), botão de retorno na página home, ajuste de layout na home, ajuste na tabela(agora é mostrada de modo fixo), corrigido um bug no grupo de botões.

--- rotas.js
+++ rotas.js
@@ -5,5 +5,16 @@ router.get('/', (req, res) => {
     res.render('home')
 });
 
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+router.post("/login", (req, res) => {
+    login.controller.login(req, res)
+});
+
+router.get("/", (req, res) => res.render("home"));
+
+router.get("/login", (req, res) => res.render("login"));
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 //# PORTA
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5004;
 
 // Express
 const app = express();
 const router = express.Router();
 
 // Body Parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // Estáticas
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,9 +24,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', path.join(__dirname, 'views', 'partials', 'layout'));
 
-
 //Rotas
 app.use('/', require('./rotas'));
+app.use('/login', require('./rotas'));
+
 
 
 // Servidor WEB
